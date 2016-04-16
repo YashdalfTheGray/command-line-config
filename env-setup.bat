c8827a84fb867637a6d6112ca9b1ca3d41b8cd7f
Yash Kulshrestha <YashdalfTheGray@users.noreply.github.com>
2016-04-16 01:56:46 -0400

    added c, l, ls and glol

--- env-setup.bat
+++ env-setup.bat
@@ -2,6 +2,9 @@
 
 REM General commands
 doskey clear=cls
+doskey c=cls
+doskey ls=dir
+doskey l=dir
 
 REM Git commands
 doskey gst=git status
@@ -9,10 +12,12 @@ doskey gaa=git add --all
 doskey ga=git add $*
 doskey gcmsg=git commit -m $*
 doskey gp=git push $*
-doskey gl=git pull $*
+doskey gpl=git pull $*
 doskey ggl=git pull origin master
 doskey gsta=git stash $*
-doskey glo=git log
+doskey gl=git log
+doskey glg=git lg
+doskey glol=git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit
 doskey gco=git checkout $*
 doskey gb=git branch $*
 doskey gd=git diff $*
